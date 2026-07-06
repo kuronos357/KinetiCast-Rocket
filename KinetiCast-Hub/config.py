c925a529979a22e2a39a543438869fc56b433907
kuronos357 <kuronos357@gmail.com>
2026-07-06 20:19:48 +0900

    あっさり成功、あとはvercelだけ

--- KinetiCast-Hub/config.py
+++ KinetiCast-Hub/config.py
@@ -1,6 +1,8 @@
-# config.py
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FIREBASE_KEY_PATH = os.path.join(BASE_DIR, "secrets", "kineticast-firebase-adminsdk.json")
 UDP_IP = "0.0.0.0"      # 全インターフェースで待ち受け
 UDP_PORT = 9870
 PACKET_FORMAT = "<Iffffff"  # timestamp_ms(I) + ax,ay,az,gx,gy,gz(f×6)
-FIREBASE_KEY_PATH = "seacrets/kineticast-firebase-adminsdk.json"  # Firebaseサービスアカウントキーのパス
 BATCH_SIZE = 100  # 1秒あたりのIMUサンプル数
